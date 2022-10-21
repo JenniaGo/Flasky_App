5afee12ea2a7f3fbff685d1d8ffced1325576696
JenniaGo <jenniagorelik@gmail.com>
2022-10-21 03:57:29 +0300

    Dockerizing

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def index():
 @app.route('/login', methods=["POST","GET"])
 def login():
     user = get_current_user()
-
     error = None
+    db = get_database()
     if request.method == "POST":
         name = request.form['name']
         password = request.form['password']
-        db = get_database()
+
         user_cursor = db.execute('select * from users where name = ?',[name])
         user = user_cursor.fetchone()
 
@@ -43,25 +43,25 @@ def login():
                 session["user"] = user["name"]
                 return redirect(url_for('dashboard'))
             else:
-                error = "Password didn't match"
+                error = "Username or Password did not match, try again"
+        else:
+            error = "Username or Password did not match, try again"
+
     return render_template('login.html', loginerror = error, user = user)
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
     user = get_current_user()
+    db = get_database()
 
     if request.method == 'POST':
-        db = get_database()
         name = request.form['name']
         password = request.form['password']
         hashed_password = generate_password_hash(password)
-
         dbusers_cur = db.execute('select * from users where name = ?', [name])
         existing_username = dbusers_cur.fetchone()
         if existing_username:
             return render_template('register.html', registererror = 'Error: Username already taken, try different username please. ', user = user)
-
-
         db.execute('insert into users (name, password) values (?,?)', [name, hashed_password])
         db.commit()
         return redirect(url_for('index'))
@@ -70,11 +70,24 @@ def register():
 @app.route('/dashboard')
 def dashboard():
     user = get_current_user()
-    return render_template('dashboard.html', user = user)
+    db = get_database()
 
-@app.route('/addneweployee')
+    emp_cur = db.execute('select * from emp')
+    allemp = emp_cur.fetchall()
+    return render_template('dashboard.html', user = user, allemp =allemp)
+
+@app.route('/addnewemployee', methods = ["POST","GET"])
 def addnewemployee():
     user = get_current_user()
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
+        db = get_database()
+        db.execute('insert into emp (name, email, phone, address) values (?,?,?,?)',[name,email,phone,address])
+        db.commit()
+        return redirect(url_for('dashboard'))
     return render_template('addnewemployee.html', user = user)
 
 @app.route('/singleemployee')
